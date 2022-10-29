60296e9cdec34213add3bd93e096f2c7b7d4d794
Unknown15082 <trangiahuy15082006@gmail.com>
2022-10-29 21:14:53 +0700

    Fix encoding to UTF-8

--- content/tex/preprocessor.py
+++ content/tex/preprocessor.py
@@ -148,7 +148,7 @@ def processwithcomments(caption, instream, outstream, listingslang):
 
     if listingslang in ['C++', 'Java']:
         hash_script = 'hash'
-        p = subprocess.Popen(['sh', 'content/contest/%s.sh' % hash_script], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        p = subprocess.Popen(['sh', 'content/contest/%s.sh' % hash_script], stdin=subprocess.PIPE, stdout=subprocess.PIPE, encoding='utf-8')
         hsh, _ = p.communicate(nsource)
         hsh = hsh.split(None, 1)[0]
         hsh = hsh + ', '
